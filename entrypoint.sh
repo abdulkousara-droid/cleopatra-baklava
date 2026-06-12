df05ae7f1fad95528431e2e0c0c0c869ad61cf1b
abdulkousara-droid <abdulkader.kousara@gmail.com>
2026-06-12 03:51:53 +0200

    removed factory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 echo "Running migrations..."
 php artisan migrate:refresh --force
 
+echo "Seeding database..."
+php artisan db:seed --force
 
 echo "Caching config..."
 php artisan config:cache
